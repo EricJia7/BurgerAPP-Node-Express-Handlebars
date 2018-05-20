56e182b3eeb380cd0ec3416e4ce704a022f9012e
Eric J <34559118+jiapurdue@users.noreply.github.com>
2018-05-20 12:43:52 -0400

    fix const SyntaxError issue

--- config/connection_mysql.js
+++ config/connection_mysql.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 
 const mysql_password = require('../keys.js')
 
-const connection;
+var connection;
 
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
